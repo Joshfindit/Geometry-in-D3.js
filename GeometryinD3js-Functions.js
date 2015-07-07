368fe81a3b6058af13ff4020d3b52b809f72b804
Josh <linkingparents@gmail.com>
2015-07-07 12:40:48 -0300

    Added a function to draw a line from two points

--- GeometryinD3js-Functions.js
+++ GeometryinD3js-Functions.js
@@ -20,6 +20,25 @@ function point(centreX, centreY, label) {
   return myPoint
 }
 
+function lineFromTwoPoints( A, B, strokeWidth, colour ) {
+  if (typeof colour == 'undefined'){
+    colour = "rgba(0,0,50,1)";
+  }
+  if (typeof strokeWidth == 'undefined'){
+    strokeWidth = 1;
+  }
+
+  var myLine = drawing.append("svg:line")
+  //x to the left
+  .attr("x1", A.attr("cx"))
+  .attr("y1", A.attr("cy"))
+  .attr("x2", B.attr("cx"))
+  .attr("y2", B.attr("cy"))
+  .attr("stroke-width", strokeWidth)
+  .style("stroke", colour);
+
+  return myLine;
+}
 
 function circle(point, radius){
   // Simply draws a circle. Does not create variables such as "circle1 = new circle(A, 50)"
