262768f24d63c485d169614bfce6191182ed70ce
Josh <linkingparents@gmail.com>
2015-07-09 22:27:30 -0300

    Brought the js in line with the master branch
    Duplicated the Geometry-in-D3.js file from master to gh-pages for use in the next blog post

--- js/GeometryinD3js-Functions.js
+++ js/GeometryinD3js-Functions.js
@@ -2,45 +2,70 @@ function point(centreX, centreY, label) {
 
   myPoint = drawing.append("circle")
   .style("stroke", "none") // Do not draw the line
-  .style("fill", "none") // Do not fill
-  .attr("r", 0)
+  .style("fill", "none")   // Do not fill
+  .attr("class", "point")  //Assign the "point" class so we can later grab/filter it
+  .attr("r", 0)            //radius = 0
   .attr("cx", centreX)
   .attr("cy", centreY)
-  .attr("label", label) //This is optional.
-                        //It follows geometry textbooks
+  .attr("label", label)    //This is optional.
+  ;                        //It follows geometry textbooks
 
   return myPoint
 }
 
-function circle(point, radius, colour){
+function circle(centrePoint, radius, colour){
   // Simply draws a circle. Returns an object such as "circle1 = new circle(A, 50)"
   if (typeof colour == 'undefined'){
     colour = "black";
   }
+  
   myCircle = drawing.append("circle")
   .style("stroke", colour)
   .style("fill", "none")
   .attr("r", radius)
-  .attr("cx", point.attr("cx"))
-  .attr("cy", point.attr("cy"))
-  .attr("label", point.attr("label"))
+  .attr("cx", centrePoint.attr("cx"))
+  .attr("cy", centrePoint.attr("cy"))
+  .attr("label", "Circle".concat(centrePoint.attr("label"))) //This returns "CircleA" if point A has a label of "A"
+  ;
 
   return myCircle;
 }
 
-function circleFilled (point, radius, colour){
-  // Simply draws a circle. Does not create variables such as "circle1 = new circle(A, 50)"
-
+function circleFilled (centrePoint, radius, colour){
+  // Simply draws a circle. Returns an object such as "circle1 = new circleFilled(A, 50)"
   if (typeof colour == 'undefined'){
     colour = "black";
   }
+  
   myFilledCircle = drawing.append("circle")
   .style("stroke", "none")
   .style("fill", colour)
   .attr("r", radius)
-  .attr("cx", point.attr("cx"))
-  .attr("cy", point.attr("cy"))
-  .attr("label", point.attr("label"));
+  .attr("cx", centrePoint.attr("cx"))
+  .attr("cy", centrePoint.attr("cy"))
+  .attr("label", "Circle".concat(centrePoint.attr("label"))) //This returns "CircleA" if point A has a label of "A"
+  ;
 
   return myFilledCircle;
+}
+
+function lineFromTwoPoints( A, B, strokeWidth, colour ) {
+  //Draws a line from two points, and returns the line
+  if (typeof colour == 'undefined'){
+    colour = "rgb(0,0,50)";
+  }
+  if (typeof strokeWidth == 'undefined'){
+    strokeWidth = 1; //This is the default anyway
+  }
+
+  var myLine = drawing.append("svg:line")
+  //x to the left
+  .attr("x1", A.attr("cx"))
+  .attr("y1", A.attr("cy"))
+  .attr("x2", B.attr("cx"))
+  .attr("y2", B.attr("cy"))
+  .attr("stroke-width", strokeWidth)
+  .style("stroke", colour);
+
+  return myLine;
 }
